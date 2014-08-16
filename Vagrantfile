61fd107fc6416fd170770162b69c487bd87ccee8
Jair Cueva Junior <Junior@Jairs-Mac-mini.local>
2014-08-16 13:33:53 -0300

    atualizaçâo

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,10 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu1404x64"
   config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.hostname = "localhost"
   config.vm.network "private_network", ip: "192.168.50.4"
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'" # avoids 'stdin: is not a tty' error.
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.synced_folder "./web/", "/vagrant/web", id: "123", group: 'www-data', owner: 'www-data', mount_options: ["dmode=775", "fmode=764"]
+  config.vm.synced_folder "../", "/var/www", id: "123", group: 'www-data', owner: 'www-data', mount_options: ["dmode=775", "fmode=764"]
+
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,53 +1,62 @@
 echo "----> Iniciando Configuração"
 
-echo "----> Atualizando Linux"
-sudo cp /vagrant/files/sources.list /etc/apt/sources.list
-apt-get update
+echo "----> Arruma diferença de LOCALE entre o terminal do MAC e o terminal da VM"
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
 
-echo "----> Upgrade Dist"
-apt-get dist-upgrade -y
+echo "----> Atualizando Linux"
+apt-get update && apt-get dist-upgrade
 
 echo "----> Instalando apache"
 apt-get install apache2 -y
 a2enmod rewrite
 
 echo "----> Instando o PHP"
-apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
-apt-get install php5-cli -y
+apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-cli -y
+cp ./files/php.ini /etc/php5/apache2/php.ini -R
 
-echo "----> Instando o PERCL"
+echo "----> Instando o PECL"
 apt-get install python-software-properties php5-dev -y
 
 echo "----> Instando o CURL"
-apt-get install php5-curl -y
+apt-get install curl php5-curl -y
+
+echo "----> Instando o XDEBUG"
+pecl install xdebug
+
+echo "----> Instando GD"
+apt-get install php5-gd -y
 
 echo "----> Instando o MYSQL"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql'
 apt-get -y install mysql-server php5-mysql -y
 
+echo "----> Arruma privilegios MYSQL"
+cp ./files/my.cnf /etc/mysql/my.cnf -R
+service mysql restart
+mysql -uroot -pmysql -e "DELETE FROM mysql.user WHERE User='root' AND Host='127.0.0.1';
+DELETE FROM mysql.user WHERE User='root' AND Host='::1';
+UPDATE mysql.user SET Host='%' WHERE Host='localhost' AND User='root';
+FLUSH PRIVILEGES;"
+
 echo "----> Instando o Memcached"
-apt-get install php5-memcached memcached -y
+# apt-get install php5-memcached memcached -y
 
 echo "----> Instando o APC"
-pecl install apc
+# pecl install apc
 
 echo "----> Instando composer"
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/bin
 
 echo "----> Instando GIT"
-apt-get install git-core -y
-apt-get install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential -y
+apt-get install git -y
 
 echo "----> Adicionando diretorio de Vhosts" 
 rm -rf /etc/apache2/sites-enabled
 ln -fs /vagrant/files/vhosts /etc/apache2/sites-enabled
 
-echo "----> Adicionando diretorio de WWW" 
-rm -rf /var/www
-ln -fs /vagrant/web /var/www
-
-echo "----> Instando dependencias" 
-apt-get -f install -y 
+echo "----> Copia PHP INFO"
+cp ./web/info /var/www -R
 
 echo "----> Reiniciando apache" 
 service apache2 restart
